6fe5ffe3911738aa681d05da446305f4b4d86939
blackoise <blackoise@googlemail.com>
2011-07-30 13:51:48 +0200

    Added optional additional timing to fix issue with not attached PuTTy windows.
    http://superputty.vanillaforums.com/discussion/28/putty-windows-are-not-attached

--- SuperPutty/frmSuperPutty.cs
+++ SuperPutty/frmSuperPutty.cs
@@ -132,6 +132,9 @@ namespace SuperPutty
             // Set automatic update check menu item
             setAutomaticUpdateCheckMenuItem();
             
+            // Set addtional timing menu item
+            setAdditionalTimingMenuItem();
+            
             // Check for updates.
             checkForUpdate(true);
             
@@ -568,6 +571,46 @@ namespace SuperPutty
 			Classes.Database.SetKeyStatic("main_form_state", this.WindowState.ToString());
 			Classes.Database.SetKeyStatic("main_form_height", this.Height.ToString());
 			Classes.Database.SetKeyStatic("main_form_width", this.Width.ToString());                              
+        }
+        
+        void AdditionalTimingClick(object sender, EventArgs e)
+        {
+        	// Get the current value from the database
+        	Classes.Database d = new SuperPutty.Classes.Database();
+        	d.Open();
+			string key = "additional_timing";
+			bool val = false;
+			val = d.GetKey(key) == "" ? false : bool.Parse(d.GetKey(key));
+			
+			// If the value is true, then set it to false and uncheck menu item,
+			// else, set it to true.
+        	if (val)
+        	{
+        		val = false;
+        	}
+        	else
+        	{
+        		val = true;
+        	}
+        	
+        	// Set the menu item check state.
+        	this.additionalTiming.Checked = val;
+
+        	// Update the database
+        	d.SetKey(key, val.ToString());
+        }
+        
+        private void setAdditionalTimingMenuItem()
+        {
+        	// Get the current value from the database
+        	Classes.Database d = new SuperPutty.Classes.Database();
+        	d.Open();
+			string key = "additional_timing";
+			bool val = false;
+			val = d.GetKey(key) == "" ? false : bool.Parse(d.GetKey(key));
+
+			// Set the checked property
+			this.additionalTiming.Checked = val;
         }
     }
 }
