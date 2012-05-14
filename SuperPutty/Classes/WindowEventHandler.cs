e5392edf12f02416d02cbfc0556dfc570ab68ed4
Anh-Kiet Ngo <anhkiet@gmail.com>
2012-05-14 02:42:16 -0700

    Refactoring code for event handling.

---
+++ SuperPutty/Classes/WindowEventHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System;
+
+namespace SuperPutty.Classes
+{
+    abstract class WindowEventHandler : IDisposable
+    {
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr
+           hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+           uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(HandleRef hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
+
+        WinEventDelegate procDelegate;
+
+        protected IntPtr m_hook = IntPtr.Zero;
+        protected frmSuperPutty m_form;
+
+        public WindowEventHandler(frmSuperPutty form)
+        {
+            m_form = form;
+        }
+
+        protected void HookEvent(uint eventType)
+        {
+            procDelegate = new WinEventDelegate(WinEventProc);
+
+            // Listen for foreground changes across all processes/threads on current desktop...
+            m_hook = SetWinEventHook(eventType, eventType, IntPtr.Zero,
+                    procDelegate, 0, 0, 0);
+        }
+
+        ~WindowEventHandler()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (this.m_hook != IntPtr.Zero)
+            {
+                UnhookWinEvent(m_hook);
+            }
+        }
+
+        protected abstract void WinEventProc(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+    }
+}
--- SuperPutty/Classes/WindowTitleTracker.cs
+++ SuperPutty/Classes/WindowTitleTracker.cs
@@ -6,56 +6,17 @@ using System.Runtime.InteropServices;
 
 namespace SuperPutty.Classes
 {
-    class WindowTitleTracker : IDisposable
+    class WindowTitleTracker : WindowEventHandler
     {
-        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType,
-            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr
-           hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
-           uint idThread, uint dwFlags);
-
-        [DllImport("user32.dll")]
-        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
-
-        [DllImport("user32.dll", CharSet=CharSet.Auto)]
-        public static extern int GetWindowTextLength(HandleRef hWnd);
-
-        [DllImport("user32.dll", CharSet=CharSet.Auto)]
-        public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
- 
         // Constants from winuser.h
         const uint EVENT_OBJECT_NAMECHANGE = 0x800C;
-        const uint EVENT_SYSTEM_FOREGROUND = 3;
-        const uint WINEVENT_OUTOFCONTEXT = 0;
 
-        WinEventDelegate procDelegate;
-
-        private IntPtr m_hook;
-        private frmSuperPutty m_form;
-
-        public WindowTitleTracker(frmSuperPutty form)
+        public WindowTitleTracker(frmSuperPutty form) : base(form)
         {
-            m_form = form;
-            procDelegate = new WinEventDelegate(WinEventProc);
-
-            // Listen for foreground changes across all processes/threads on current desktop...
-            m_hook = SetWinEventHook(EVENT_OBJECT_NAMECHANGE, EVENT_OBJECT_NAMECHANGE, IntPtr.Zero,
-                    procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+            HookEvent(EVENT_OBJECT_NAMECHANGE);
         }
 
-        ~WindowTitleTracker()
-        {
-            Dispose();
-        }
-
-        public void Dispose()
-        {
-            UnhookWinEvent(m_hook);
-        }
-
-        private void WinEventProc(IntPtr hWinEventHook, uint eventType,
+        protected override void WinEventProc(IntPtr hWinEventHook, uint eventType,
             IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
         {
             if (hwnd != this.m_form.Handle && this.m_form.ContainsChild(hwnd))
