dcaa29411157d92b30205276fd7bf4c1bc189709
Anh-Kiet Ngo <anhkiet@gmail.com>
2012-05-19 23:43:51 -0700

    Adding WS_EXSTYLE constants.

--- SuperPutty/Classes/WinAPI.cs
+++ SuperPutty/Classes/WinAPI.cs
@@ -17,13 +17,6 @@ namespace SuperPutty.Classes
         public const int GWL_STYLE = (-16);
         public const int GWL_EXSTYLE = (-20);
 
-        public const int WM_CLOSE = 0x10;
-        public const int WS_CAPTION = 0x00C00000;
-        public const int WS_EX_NOACTIVATE = 0x08000000;
-        public const int WS_BORDER = 0x00800000;
-        public const int WS_VSCROLL = 0x00200000;
-        public const int WS_THICKFRAME = 0x00040000;
-
         public delegate IntPtr LowLevelKeyboardProc(int nCode, UIntPtr wParam, IntPtr lParam);
         public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
 
@@ -160,6 +153,37 @@ namespace SuperPutty.Classes
             WS_TILEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
             WS_VISIBLE = 0x10000000,
             WS_VSCROLL = 0x00200000;
+        }
+
+        public static class ExtendedWindowStyles
+        {
+            public static readonly Int32
+            WS_EX_ACCEPTFILES = 0x00000010,
+            WS_EX_APPWINDOW = 0x00040000,
+            WS_EX_CLIENTEDGE = 0x00000200,
+            WS_EX_COMPOSITED = 0x02000000,
+            WS_EX_CONTEXTHELP = 0x00000400,
+            WS_EX_CONTROLPARENT = 0x00010000,
+            WS_EX_DLGMODALFRAME = 0x00000001,
+            WS_EX_LAYERED = 0x00080000,
+            WS_EX_LAYOUTRTL = 0x00400000,
+            WS_EX_LEFT = 0x00000000,
+            WS_EX_LEFTSCROLLBAR = 0x00004000,
+            WS_EX_LTRREADING = 0x00000000,
+            WS_EX_MDICHILD = 0x00000040,
+            WS_EX_NOACTIVATE = 0x08000000,
+            WS_EX_NOINHERITLAYOUT = 0x00100000,
+            WS_EX_NOPARENTNOTIFY = 0x00000004,
+            WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,
+            WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST,
+            WS_EX_RIGHT = 0x00001000,
+            WS_EX_RIGHTSCROLLBAR = 0x00000000,
+            WS_EX_RTLREADING = 0x00002000,
+            WS_EX_STATICEDGE = 0x00020000,
+            WS_EX_TOOLWINDOW = 0x00000080,
+            WS_EX_TOPMOST = 0x00000008,
+            WS_EX_TRANSPARENT = 0x00000020,
+            WS_EX_WINDOWEDGE = 0x00000100;
         }
 
         /// <summary>Enumeration of the different ways of showing a window using
